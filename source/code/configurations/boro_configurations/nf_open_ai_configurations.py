0a0285df61b5cade627291330f571583521be37f
Mesbah Khan <khanm@ontoledgy.io>
2024-09-23 21:46:00 +0100

    updated requirements

---
+++ source/code/configurations/boro_configurations/nf_open_ai_configurations.py
@@ -0,0 +1,12 @@
+class NfOpenAiConfigurations:
+    default_string_empty = \
+        str()
+
+    OPEN_AI_API_KEY = \
+        default_string_empty
+
+    OPEN_AI_ORGANISATION_KEY = \
+        default_string_empty
+
+    OPEN_AI_PROJECT_KEY = \
+        default_string_empty
---
+++ source/release_management/generate_requirements.py
@@ -0,0 +1,12 @@
+import importlib.metadata
+
+
+def generate_requirements_txt(filename='requirements.txt'):
+    with open(filename, 'w') as f:
+        for dist in importlib.metadata.distributions():
+            f.write(f"{dist.metadata['Name']}=={dist.version}\n")
+
+
+if __name__ == "__main__":
+    generate_requirements_txt()
+    print("Requirements saved to requirements.txt")
