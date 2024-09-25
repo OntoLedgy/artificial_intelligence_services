17a2386e91e46afc91ce80585f063cfb772fa1f5
xibertao <xibertao@borogroup.co.uk>
2024-09-25 09:49:12 +0100

    extract configurations

--- source/b_code/configurations/boro_configurations/nf_general_configurations.py
+++ source/b_code/configurations/boro_configurations/nf_general_configurations.py
@@ -8,6 +8,28 @@ class NfGeneralConfigurations:
     
     RECURSIVE_CHARACTER_TEXTSPLITTER_CHUNK_OVERLAP = \
         50
+    
+    DEFAULT_DATA_CHUNK_SIZE_FOR_TRAINING = \
+        512
+    
+    DEFAULT_TRUNCATE_CONTEXT_MAX_TOKENS = \
+        4000
+    
+    HUGGING_FACE_MODEL_NAME = \
+        'accounting_fine_tuned'
+    
+    # def test_text_generation(self):
+    #
+    #     model_path = r'data/outputs/models/'
+    #     model_name = "accounting_fine_tuned"
+    
+    # def truncate_context(
+    #         context,
+    #         max_tokens = 4000)
+    
+    # def prepare_data_for_training(
+    #         texts,
+    #         chunk_size = 512
 
 # model.generate(
 #         input_ids,
--- source/b_code/configurations/boro_configurations/nf_open_ai_configurations.py
+++ source/b_code/configurations/boro_configurations/nf_open_ai_configurations.py
@@ -11,7 +11,13 @@ class NfOpenAiConfigurations:
     OPEN_AI_PROJECT_KEY = \
         default_string_empty
 
-    OPEN_AI_MODEL_NAME = \
+    OPEN_AI_MODEL_NAME_1 = \
+        'gpt-4o-mini'
+    
+    OPEN_AI_MODEL_NAME_2 = \
+        'gpt-3.5-turbo'
+    
+    OPEN_AI_MODEL_NAME_3 = \
         'gpt-4o'
 
     OPEN_AI_TEMPERATURE = \
@@ -19,3 +25,14 @@ class NfOpenAiConfigurations:
 
     OPEN_AI_MAX_TOKENS = \
         1000
+    
+    DEFAULT_MAX_TRUNCATE_CONTEXT_TOKENS = \
+        12000
+    
+    
+    # def get_response(
+    #         query,
+    #         client,
+    #         model_name = NfOpenAiConfigurations.OPEN_AI_MODEL_NAME_2,
+    #         input_file = 'retrieved_articles.txt',
+    #         max_context_tokens = 12000):
