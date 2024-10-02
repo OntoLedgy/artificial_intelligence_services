af8720c1738f65a6f7d827ee2e271f6a40bf24ab
Oscar Xiberta Soto <54765866+OscarXSoto@users.noreply.github.com>
2024-10-02 12:37:46 +0100

    Merge pull request #24
    * hugging face restructuring - first component in progress.
    
    * hugging face restructuring - first component - storage classes created.
    
    * hugging face restructuring - first component - storage classes created.
    
    * hugging face restructuring - first component - storage classes modified.
    
    * hugging face restructuring - first component - minor changes
    
    * Merge branch 'boro_dev' into boro/oxi/hugging_face_restructuring
    
    * hugging face restructuring - first component - minor changes
    
    * hugging face restructuring - minor changes.
    
    * Merge pull request #23 from OntoLedgy/boro/oxi/hugging_face_restructu…
    
    * graph retriever componentisation - Orchestrator started. In progress.
    
    * Merge branch 'boro_dev' into boro/oxi/graph_retriever_componentisation
    
    * graph retriever componentisation - First design of the componentised …
    
    * Merge pull request #25 from OntoLedgy/boro/oxi/graph_retriever_compon…

---
+++ source/b_code/services/data_preparation/chunked_texts_getter.py
@@ -0,0 +1,45 @@
+import os
+
+from configurations.boro_configurations.nf_general_configurations import (
+    NfGeneralConfigurations,
+)
+from configurations.constants import PDF_FILE_EXTENSION
+from services.data_preparation.objects.chunked_texts import ChunkedTexts
+from services.data_preparation.objects.texts import Texts
+from services.data_preparation.pdf_services import load_pdfs
+
+
+def get_chunked_texts(
+        source_texts_folder_path: str,
+        chunked_texts_output_file_path: str = None,
+        extension: str = PDF_FILE_EXTENSION,
+        chunk_size: int = NfGeneralConfigurations.DEFAULT_DATA_CHUNK_SIZE_FOR_TRAINING) \
+    -> ChunkedTexts:
+    # TODO: Only PDF implemented at the moment
+    if extension == PDF_FILE_EXTENSION:
+        source_texts = \
+            load_pdfs(
+                    directory=source_texts_folder_path)
+    
+    else:
+        source_texts = \
+            list()
+        
+    texts = \
+        Texts(
+            source_texts=source_texts,
+            output_folder_path=os.path.dirname(chunked_texts_output_file_path))
+    
+    chunked_texts = \
+        ChunkedTexts(
+            texts=texts,
+            chunk_size=chunk_size,
+            output_file_path=chunked_texts_output_file_path)
+
+    if chunked_texts_output_file_path:
+        texts.export_to_csv()
+        
+        chunked_texts.export_to_jsonl()  # TODO: move the output folder to the class parameters - DONE
+
+    return \
+        chunked_texts
--- source/b_code/services/orchestrators/text_generation_orchestrator.py
+++ source/b_code/services/orchestrators/text_generation_orchestrator.py
@@ -3,9 +3,30 @@ from services.llms.text_generators import generate_text_using_pipeline
 from services.model_management.model_loader import load_model
 
 
-def orchestrate_text_generation(model_path: str, model_name: str, prompt: str):
-    model, tokeniser = load_model(model_name=model_name, model_path=model_path)
-
-    generate_text_using_pipeline(model=model, tokenizer=tokeniser, input_text=prompt)
-
-    generate_text_using_model(model=model, tokenizer=tokeniser, input_text=prompt)
+def orchestrate_text_generation(
+        model_path: str,
+        model_name: str,
+        prompt: str) -> dict:
+    model, tokeniser = load_model(
+        model_name=model_name,
+        model_path=model_path)
+    
+    generated_text_using_pipeline = \
+        generate_text_using_pipeline(
+            model=model,
+            tokenizer=tokeniser,
+            input_text=prompt)
+    
+    generated_text_using_model = \
+        generate_text_using_model(
+            model=model,
+            tokenizer=tokeniser,
+            input_text=prompt)
+    
+    generated_texts_dictionary = {
+        "generated_text_using_pipeline": generated_text_using_pipeline,
+        "generated_text_using_model"   : generated_text_using_model,
+        }
+    
+    return \
+        generated_texts_dictionary
