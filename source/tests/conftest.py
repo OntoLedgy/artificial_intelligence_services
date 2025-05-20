70d4e5ca9e3368b4adeba345f462fa94ddd0ec1d
Mesbah Khan <khanm@ontoledgy.io>
2025-05-20 09:32:33 +0100

    adding

--- source/tests/conftest.py
+++ source/tests/conftest.py
@@ -2,3 +2,9 @@ from tests.fixtures.configurations import *  # noqa: F403
 from tests.fixtures.open_ai_service import *  # noqa: F403
 from tests.fixtures.paths import * # noqa: F403
 from tests.fixtures.test_data import * # noqa: F403
+import pytest
+
+
+@pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
