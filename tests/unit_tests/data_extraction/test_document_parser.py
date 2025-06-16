0d0c72acbcd2f64c6b1a0090dcd64cad0f36d903
wabhatti <wabhatti@gmail.com>
2025-06-16 21:01:43 +0100

    first commit

--- tests/unit_tests/data_extraction/test_document_parser.py
+++ tests/unit_tests/data_extraction/test_document_parser.py
@@ -13,12 +13,14 @@ class TestDocumentParsing():
             inputs_folder_absolute_path):
         self.pdf_path = os.path.join(
                 inputs_folder_absolute_path,
-                r"D:\S\python\artificial_intelligence_services\tests\data\inputs\pdf\accounting\Blums_Conceptualizing-resources.pdf"
+#                r"D:\Source\artificial_intelligence_services\tests\data\inputs\pdf\accounting\Blums_Conceptualizing-resources.pdf"
+                r"D:\Data\PEH\09.pdf"
                 )
         
     def test_parse_document(self):
         parser = DocumentParser()
         doc_structure = parser.parse(
-                r"D:\S\python\artificial_intelligence_services\tests\data\inputs\pdf\accounting\Blums_Conceptualizing-resources.pdf",
+#                r"D:\Source\artificial_intelligence_services\tests\data\inputs\pdf\accounting\Blums_Conceptualizing-resources.pdf",
+                r"D:\Data\PEH\09.pdf",
                 )
         doc_structure.print_document_structure()
